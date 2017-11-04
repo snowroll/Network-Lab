3a3515dcc77ee5e47fc56bf42589852de306a8db
柴华君 <chaihj15@mails.tsinghua.edu.cn>
2017-11-04 09:56:42 +0800

    change one more

--- slide.cpp
+++ slide.cpp
@@ -1,4 +1,5 @@
-//#include "sysinclude.h"
+#include "sysinclude.h"
+#include <queue>
 #include <iostream>
 using namespace std;
 
@@ -7,109 +8,101 @@ extern void SendFRAMEPacket(unsigned char* pData, unsigned int len);
 #define WINDOW_SIZE_STOP_WAIT 1
 #define WINDOW_SIZE_BACK_N_FRAME 4
 
-typedef enum{data, ack, nak}frame_kind;
-
-typedef struct frame_head{
-	frame_kind kind;
-	unsigned int seq;
-	unsigned int ack;
-	unsigned char data[100];
+typedef enum {data,ack,nak} frame_kind; 
+typedef struct frame_head
+{
+	frame_kind kind;              
+	unsigned int seq;             
+	unsigned int ack;             
+	unsigned char data[100];      
 };
-
-typedef struct frame{
-	frame_head head;
-	unsigned int size;
+typedef struct frame
+{
+	frame_head head;              
+	unsigned int size;            
 };
+typedef struct packet{
+	frame* one_frame;
+	int len;
+};
+queue<packet> buffer;
+int size = 0;  //目前窗口的长度
+bool yes_no = true;
 
-frame frame_buffer[50];
-unsigned int tail_f = 0, head_f = 0, upper_bound = 0;
 
-int stud_slide_window_stop_and_wait(char* pBuffer, int bufferSize, UINT8 messageType){
-	switch(messageType){
-		case MSG_TYPE_TIMEOUT:{
-			for(int i = tail_f; i < upper_bound; i++){
-				int pos = i % 50;
-				SendFRAMEPacket((unsigned char*)(&frame_buffer[pos]), frame_buffer[pos].size);;
-			}
-			return 0;
-		}
-		case MSG_TYPE_SEND:{
-			int head_pos = head_f % 50;
-			frame_buffer[head_pos].head = (frame*)pBuffer;
-			frame_buffer[head_pos].size = bufferSize;
-			head_f++;
-			if(head_f - tail_f <= WINDOW_SIZE_STOP_WAIT){
-				SendFRAMEPacket((unsigned char*)(&frame_buffer[head_pos]), (unsigned int)bufferSize);
-				upper_bound++;
-			}
-			return 0;
-		}
-		case MSG_TYPE_RECEIVE:{
-			frame* rec_frame = (frame*)pBuffer;
-			unsigned int rec_ack = ntohl(rec_frame->head.ack);
-			for(int i = tail_f; i < upper_bound; i++){
-				int cur_pos = i % 50;
-				unsigned int cur_ack = ntohl(frame_buffer[cur_pos].head.seq);
-				if(cur_ack <= rec_ack){
-					tail_f++;
-					if(upper_bound < head_f){
-						int upper_pos = upper_bound % 50;
-						SendFRAMEPacket((unsigned char*)(&frame_buffer[upper_pos]), frame_buffer[upper_pos].size);
-						upper_bound++;
-					}
-					else break;
+int stud_slide_window_stop_and_wait(char *pBuffer, int bufferSize, UINT8 messageType)
+{
+	packet cur_frame_packet;  //将帧打包
+	cur_frame_packet.one_frame = new frame();
+	cur_frame_packet.one_frame = (frame*)pBuffer;
+	cur_frame_packet.len = bufferSize;
+
+	if(messageType == MSG_TYPE_SEND){
+		packet.push_back(cur_frame_packet);
+		if(yes_no){  //停等协议，窗口空闲即可发送
+			yes_no = false;  //等待确认的过程，不能再次发送
+			SendFRAMEPacket((unsigned char*)pBuffer, bufferSize);  //发送当前帧
+		} 
+	}
+	else if(messageType == MSG_TYPE_RECEIVE){
+		unsigned int ack = cur_frame_packet.one_frame->head.seq;
+		if(buffer.size() > 0){
+			if(ack == buffer.front().one_frame->head.seq){
+				buffer.pop();
+				yes_no = true;  //接受到帧，可以再次发送
+				if(buffer.size() > 0){  //此时buffer还有缓存帧的话，直接发送
+					SendFRAMEPacket((unsigned char*)buffer.front().one_frame, buffer.front().len);
+					yes_no = false;
 				}
 			}
-			return 0;
+
 		}
-		default: break;
 	}
-	return -1;
+	else if(messageType == MSG_TYPE_TIMEOUT){
+		SendFRAMEPacket((unsigned char*)buffer.front().one_frame, buffer.front().len);
+	}
+	return 0;
 }
 
-int stud_slide_window_back_n_frame(char* pBuffer, int bufferSize, UINT8 messageType){
-	switch(messageType){
-		case MSG_TYPE_TIMEOUT:{
-			for(int i = tail_f; i < upper_bound; i++){
-				int pos = i % 50;
-				SendFRAMEPacket((unsigned char*)(&frame_buffer[pos]), frame_buffer[pos].size);;
-			}
+int stud_slide_window_back_n_frame(char *pBuffer, int bufferSize, UINT8 messageType)
+{
+	/*static int head = 1;
+	static int tail = 1;
+	static int size = 0;
+	static char list[30][100];
+	static int list_size[30];	
+	if(messageType == MSG_TYPE_SEND){
+		list_size[tail] = bufferSize;
+		for(int i = 0 ; i < list_size[tail]; i++){
+			list[tail][i] = pBuffer[i];
 		}
-		case MSG_TYPE_SEND:{
-			int head_pos = head_f % 50;
-			frame_buffer[head_pos].head = (frame*)pBuffer;
-			frame_buffer[head_pos].size = bufferSize;
-			head_f++;
-			if(head_f - tail_f <= WINDOW_SIZE_BACK_N_FRAME){
-				frame* tmp_frame = (frame*)pBuffer;
-				SendFRAMEPacket((unsigned char*)tmp_frame, (unsigned int)bufferSize);
-				upper_bound++;
-			}
-			return 0;
+		tail++;
+		if(size >= WINDOW_SIZE_BACK_N_FRAME){
+			return -1;
 		}
-		case MSG_TYPE_RECEIVE:{
-			frame* rec_frame = (frame*)pBuffer;
-			unsigned int rec_ack = ntohl(rec_frame->head.ack);
-			for(int i = tail_f; i < upper_bound; i++){
-				int cur_pos = i % 50;
-				unsigned int cur_ack = ntohl(frame_buffer[cur_pos].head.seq);
-				if(cur_ack <= rec_ack){
-					tail_f++;
-					if(upper_bound < head_f){
-						int upper_pos = upper_bound % 50;
-						SendFRAMEPacket((unsigned char*)(&frame_buffer[upper_pos]), frame_buffer[upper_pos].size);
-						upper_bound++;
-					}
-					else break;
-				}
-			}
-			return 0;
-		}
-		default: break;
+		size++;
+		SendFRAMEPacket((unsigned char *)pBuffer, bufferSize);
 	}
-	return -1;
+	else if(messageType == MSG_TYPE_RECEIVE){
+		frame *packet = (frame *)pBuffer;
+		while(head <= ntohl(packet->head.ack)){
+			head++;
+			size--;
+		}
+		for (int i = head + size; i < tail && size < WINDOW_SIZE_BACK_N_FRAME; i++){
+			SendFRAMEPacket((unsigned char *)list[i], list_size[i]);
+			size++;
+		}
+	}
+	else if(messageType == MSG_TYPE_TIMEOUT){
+		for(int i = 0; i < tail - head && i < WINDOW_SIZE_BACK_N_FRAME; i++){
+			SendFRAMEPacket((unsigned char *)list[head + i], list_size[head + i]);
+		}
+	}*/
+	return 0;
 }
 
-int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 messageType){
+int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 messageType)
+{
 	return 0;
 }
